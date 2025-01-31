60675b8389c80dd34a04a5a57d81e6d357f81e62
TumbleWeedBro <munashwengz@gmail.com>
2025-01-31 21:04:58 +0200

    homepage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -45,7 +45,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(toolbarHeight:2,),
       body: _pages[_selectedIndex],
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _selectedIndex,
